880717a7a67a397d517feb77d8dbe9d9f925965a
Grandclosing <vukpetrovic1@yahoo.com>
2020-11-09 20:01:29 -0500

    Implemented mobile navigation and hamburger button. I need to clean up the code now

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+let hamburgerOpen = false;
+
+function toggleHamburger(elem) {
+    hamburgerOpen = !hamburgerOpen;
+    elem.classList.toggle('is-active');
+
+    let mobileMenu = document.getElementById('mobile-navigation');
+    mobileMenu.style.display = hamburgerOpen ? 'block' : 'none';
+}
