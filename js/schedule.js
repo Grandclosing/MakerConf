19a9b26864ab7d8650b4c7eeb535529bc37fe7c2
Grandclosing <vukpetrovic1@yahoo.com>
2020-11-12 10:12:32 -0500

    Added some more responsive CSS to each page; tested on different mobile device dimensions. Finished schedule page; working on sponsor page next

--- js/schedule.js
+++ js/schedule.js
@@ -9,6 +9,7 @@ let sundayElement;
 let sundayPosition;
 
 let lastScrollPosition = 0;
+const scrollPositionOffset = 300; 
 let ticking = false;
 
 const FRIDAY = 0;
@@ -44,20 +45,13 @@ window.onload = function() {
 function setDayBorder() {
     let newDay; 
 
-    if(lastScrollPosition >= 0 && lastScrollPosition < saturdayPosition) {
+    if(lastScrollPosition >= 0 && lastScrollPosition < saturdayPosition - scrollPositionOffset) {
         newDay = FRIDAY;
-    } else if(lastScrollPosition >= saturdayPosition && lastScrollPosition < sundayPosition) {
+    } else if(lastScrollPosition >= saturdayPosition - scrollPositionOffset && lastScrollPosition < sundayPosition - scrollPositionOffset) {
         newDay = SATURDAY;
-    } else if(lastScrollPosition >= sundayPosition) {
+    } else if(lastScrollPosition >= sundayPosition - scrollPositionOffset) {
         newDay = SUNDAY;
     }
-
-    console.log(`lastScrollPosition: ${lastScrollPosition}, 
-    fridayPosition: ${fridayPosition}, 
-    saturdayPosition: ${saturdayPosition},
-    sundayPosition: ${sundayPosition},
-    newDay: ${newDay},
-    currentDay: ${currentDay}`);
 
     if(newDay == currentDay) return; // nothing to do
 
@@ -65,19 +59,16 @@ function setDayBorder() {
 
     switch(currentDay) {
         case FRIDAY:
-            console.log("styling for friday");
             fridayElement.style.borderLeftColor = "#C82586";
             saturdayElement.style.borderLeftColor = "transparent";
             sundayElement.style.borderLeftColor = "transparent";
             break;
         case SATURDAY: 
-            console.log("styling for saturday");
             fridayElement.style.borderLeftColor = "transparent";
             saturdayElement.style.borderLeftColor = "#C82586";
             sundayElement.style.borderLeftColor = "transparent";
             break;
         case SUNDAY:
-            console.log("styling for sunday");
             fridayElement.style.borderLeftColor = "transparent";
             saturdayElement.style.borderLeftColor = "transparent";
             sundayElement.style.borderLeftColor = "#C82586";
