a2f39f3c64665a8b1373db44c65ae4b2759b7871
Grandclosing <vukpetrovic1@yahoo.com>
2020-11-17 21:12:42 -0500

    Added prettier to have consistent code styling throughout the project. I still need to add the git hook.

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,9 @@
-
 let hamburgerOpen = false;
 
 function toggleHamburger(elem) {
-    hamburgerOpen = !hamburgerOpen;
-    elem.classList.toggle('is-active');
+  hamburgerOpen = !hamburgerOpen;
+  elem.classList.toggle("is-active");
 
-    let mobileMenu = document.getElementById('mobile-navigation');
-    mobileMenu.style.display = hamburgerOpen ? 'block' : 'none';
+  let mobileMenu = document.getElementById("mobile-navigation");
+  mobileMenu.style.display = hamburgerOpen ? "block" : "none";
 }
--- js/schedule.js
+++ js/schedule.js
@@ -1,4 +1,3 @@
-
 let fridayElement;
 let fridayPosition;
 
@@ -9,7 +8,7 @@ let sundayElement;
 let sundayPosition;
 
 let lastScrollPosition = 0;
-const scrollPositionOffset = 300; 
+const scrollPositionOffset = 300;
 let ticking = false;
 
 const FRIDAY = 0;
@@ -17,61 +16,70 @@ const SATURDAY = 1;
 const SUNDAY = 2;
 let currentDay = FRIDAY;
 
-window.onload = function() {
-    let schedule = document.getElementById('schedule-container');
+window.onload = function () {
+  let schedule = document.getElementById("schedule-container");
 
-    fridayElement = document.getElementsByClassName('day')[0];
-    saturdayElement = document.getElementsByClassName('day')[1];
-    sundayElement = document.getElementsByClassName('day')[2];
+  fridayElement = document.getElementsByClassName("day")[0];
+  saturdayElement = document.getElementsByClassName("day")[1];
+  sundayElement = document.getElementsByClassName("day")[2];
 
-    fridayPosition = document.getElementById('friday').getBoundingClientRect().top;
-    saturdayPosition = document.getElementById('saturday').getBoundingClientRect().top;
-    sundayPosition = document.getElementById('sunday').getBoundingClientRect().top;
+  fridayPosition = document.getElementById("friday").getBoundingClientRect()
+    .top;
+  saturdayPosition = document.getElementById("saturday").getBoundingClientRect()
+    .top;
+  sundayPosition = document.getElementById("sunday").getBoundingClientRect()
+    .top;
 
-    schedule.addEventListener('scroll', function(e) {
-        lastScrollPosition = schedule.scrollTop;
+  schedule.addEventListener("scroll", function (e) {
+    lastScrollPosition = schedule.scrollTop;
 
-        if(!ticking) {
-            window.requestAnimationFrame(function() {
-                setDayBorder();
-                ticking = false;
-            });
+    if (!ticking) {
+      window.requestAnimationFrame(function () {
+        setDayBorder();
+        ticking = false;
+      });
 
-            ticking = true;
-        }
-    });
-}
+      ticking = true;
+    }
+  });
+};
 
 function setDayBorder() {
-    let newDay; 
+  let newDay;
 
-    if(lastScrollPosition >= 0 && lastScrollPosition < saturdayPosition - scrollPositionOffset) {
-        newDay = FRIDAY;
-    } else if(lastScrollPosition >= saturdayPosition - scrollPositionOffset && lastScrollPosition < sundayPosition - scrollPositionOffset) {
-        newDay = SATURDAY;
-    } else if(lastScrollPosition >= sundayPosition - scrollPositionOffset) {
-        newDay = SUNDAY;
-    }
+  if (
+    lastScrollPosition >= 0 &&
+    lastScrollPosition < saturdayPosition - scrollPositionOffset
+  ) {
+    newDay = FRIDAY;
+  } else if (
+    lastScrollPosition >= saturdayPosition - scrollPositionOffset &&
+    lastScrollPosition < sundayPosition - scrollPositionOffset
+  ) {
+    newDay = SATURDAY;
+  } else if (lastScrollPosition >= sundayPosition - scrollPositionOffset) {
+    newDay = SUNDAY;
+  }
 
-    if(newDay == currentDay) return; // nothing to do
+  if (newDay == currentDay) return; // nothing to do
 
-    currentDay = newDay; 
+  currentDay = newDay;
 
-    switch(currentDay) {
-        case FRIDAY:
-            fridayElement.style.borderLeftColor = "#C82586";
-            saturdayElement.style.borderLeftColor = "transparent";
-            sundayElement.style.borderLeftColor = "transparent";
-            break;
-        case SATURDAY: 
-            fridayElement.style.borderLeftColor = "transparent";
-            saturdayElement.style.borderLeftColor = "#C82586";
-            sundayElement.style.borderLeftColor = "transparent";
-            break;
-        case SUNDAY:
-            fridayElement.style.borderLeftColor = "transparent";
-            saturdayElement.style.borderLeftColor = "transparent";
-            sundayElement.style.borderLeftColor = "#C82586";
-            break;
-    }
+  switch (currentDay) {
+    case FRIDAY:
+      fridayElement.style.borderLeftColor = "#C82586";
+      saturdayElement.style.borderLeftColor = "transparent";
+      sundayElement.style.borderLeftColor = "transparent";
+      break;
+    case SATURDAY:
+      fridayElement.style.borderLeftColor = "transparent";
+      saturdayElement.style.borderLeftColor = "#C82586";
+      sundayElement.style.borderLeftColor = "transparent";
+      break;
+    case SUNDAY:
+      fridayElement.style.borderLeftColor = "transparent";
+      saturdayElement.style.borderLeftColor = "transparent";
+      sundayElement.style.borderLeftColor = "#C82586";
+      break;
+  }
 }
